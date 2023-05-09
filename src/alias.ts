ae0ae39ae0ebba2fb327ddd64e729be79bd2fd6b
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-10 05:06:25 +0600

    object type alias

---
+++ src/alias.ts
@@ -0,0 +1,23 @@
+type WorkInfoType = {
+    workName:string;
+    timeInHour:number;
+    workType:string
+};
+
+const learningWork:WorkInfoType={
+    workName:"Learning",
+    timeInHour:3,
+    workType:"In House/Remote"
+};
+
+const officeWork:WorkInfoType={
+    workName:"Office", 
+    timeInHour:8,
+    workType:"In Office"
+};
+
+const projectOrPartTime={
+    workName:"Project or Part Time",
+    timeInHour:3,
+    workType:"Remote"
+}
