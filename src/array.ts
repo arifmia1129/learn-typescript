d2ef948c3ae0bba5504a924ffa6a8ce5e5e808b7
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-09 07:08:34 +0600

    array in typescript

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+// string type array
+const names:string[] = ["Binu", "Arif", "Shahin"];
+
+// number type array
+const rollNumbers:number[]=[1, 2, 3];
+
+// tuple in array
+const student:[string, number] = ["Arif", 123];
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const names:string[]=[
-    "Binu", "Arif", "Shahin"
-]
-
-const rollNumbers:number[]=[
-    1, 2, 3, 4, 5
-]
-
-const student:[string, number]=["Arif", 123];
