c22b1a0d7f96a8cadf3543963b42c167e133c4ff
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-09 07:05:09 +0600

    object in typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const names:string[]=[
+    "Binu", "Arif", "Shahin"
+]
+
+const rollNumbers:number[]=[
+    1, 2, 3, 4, 5
+]
+
+const student:[string, number]=["Arif", 123];
---
+++ src/object.ts
@@ -0,0 +1,13 @@
+const user:{
+   readonly company:string;
+    name:string;
+    age:number;
+    isMarried:boolean;
+    wife?:string
+} = {
+    company:"Hello Arif",
+    name:"Arif",
+    age:21,
+    isMarried:true,
+    wife:"Binu"
+}
