e36684cac4c398bca3f6ec606682a95530a1c00d
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-10 06:29:26 +0600

    ternary operator, nulish coalesnace operator

--- src/question-mark.ts
+++ src/question-mark.ts
@@ -3,3 +3,33 @@ const myAge:number = 17;
 const isAdult = myAge >= 18 ? "Yes" : "No";
 
 console.log(isAdult);
+
+// nulish coalesnace operator
+
+const isAuthenticatedUser = "";
+
+const userName = isAuthenticatedUser ?? "Guest";
+
+console.log({userName});
+
+
+type Employee = {
+    name:string,
+    id:number,
+    address:{
+        city:string,
+        home?:string
+    }
+}
+
+const employee:Employee = {
+    name:"Arif",
+    id:123,
+    address:{
+        city:"Gaibandha"
+    }
+}
+
+const employeeHome = employee?.address?.home ?? "No Home";
+
+console.log({employeeHome});
